0725da3167881a1b1eee9e08e83932f61a84ae4a
SelamZem <selamawitzemecha14@gmail.com>
2025-12-09 10:09:40 +0300

    added domain folder

--- Domain/domain.go
+++ Domain/domain.go
@@ -5,17 +5,17 @@ import "time"
 // Domain models are independent of infrastructure and external libraries.
 
 type User struct {
-    ID        string    `json:"id"`
-    Username  string    `json:"username"`
-    Password  string    `json:"password,omitempty"` // hashed
-    Role      string    `json:"role"`
-    CreatedAt time.Time `json:"created_at"`
+	ID        string    `json:"id"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password,omitempty"` // hashed
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type Task struct {
-    ID          string `json:"id"`
-    Title       string `json:"title"`
-    Description string `json:"description"`
-    DueDate     string `json:"due_date"`
-    Status      string `json:"status"`
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	DueDate     string `json:"due_date"`
+	Status      string `json:"status"`
 }
